4226f497ae273d29cac096b7a93ff0742bc16754
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 10:40:12 +0200

    Update AD-User-Retail.ps1

--- AD/AD-User-Retail.ps1
+++ AD/AD-User-Retail.ps1
@@ -1,2 +1,2 @@
-﻿get-aduser -filter {Office-Subdepartment -like "Retail Channel"} -Properties Company, Name, EmployeeID, Office-Subgroup, msRTCSIP-UserEnabled | where {($_.'Office-Subgroup' -eq "Skanska")} | select name | Sort-Object name
+﻿get-aduser -filter {Office-Subdepartment -like "Retail Channel"} -Properties Company, Name, EmployeeID, Office-Subgroup, msRTCSIP-UserEnabled | where {($_.'Office-Subgroup' -eq "Finance")} | select name | Sort-Object name
 
