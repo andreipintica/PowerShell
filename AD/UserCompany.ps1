71e9aaeb65c70647e245e3a3def56b4576e09958
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 10:48:35 +0200

    Update UserCompany.ps1

--- AD/UserCompany.ps1
+++ AD/UserCompany.ps1
@@ -1,1 +1,1 @@
-﻿get-aduser -filter {Company -like "OMY"} -Properties Company, Name, EmployeeID | select Company, Name, EmployeeID
+﻿get-aduser -filter {Company -like "MyCompany"} -Properties Company, Name, EmployeeID | select Company, Name, EmployeeID
