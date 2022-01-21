de3b0ef48036f5e0ff9d4a90f968cac9da9b5022
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 09:33:24 +0200

    Add files via upload

---
+++ AD/AD-Count-Active-Employees.ps1
@@ -0,0 +1,53 @@
+﻿$start_date =  get-date('11/01/2020') -Format 'yyyy-MM-dd HH:mm:ss'
+$end_date =  get-date('11/30/2020') -Format 'yyyy-MM-dd HH:mm:ss'
+
+
+
+$users = get-aduser -Filter * -Properties accountexpires,employeeid,whencreated,company
+$users = $users | ? {$_.distinguishedname -notlike "*,OU=AdministrativeAccounts,DC=office"}
+$usergroup = @()
+
+foreach ($user in $users) {
+  $lngValue = $User.AccountExpires
+  [int]$marca = $user.employeeid
+
+  if ($marca -gt 0 -and $user.whencreated -lt $end_Date) {
+    # A value of 0 or 2^63-1 means never.
+    If ($lngValue -gt [DateTime]::MaxValue.Ticks)
+    {
+      $lngValue = 0
+    }
+    # Convert 64-bit integer into DateTime.
+    $Date = [DateTime]$lngValue
+    If ($Date -eq 0)
+    {
+          $entry = new-object psobject -Property @{
+                      User = $user.samaccountname
+                      Company = $user.company
+                      }
+          $usergroup += $entry
+    }
+    Else
+    {
+      # Convert Active Directory ticks to PowerShell ticks.
+      # Also convert from UTC to local time.
+      $AcctExpires = $Date.AddYears(1600).ToLocalTime()
+      if ($AcctExpires -gt $start_date) {
+          $entry = new-object psobject -Property @{
+                User = $user.samaccountname
+                Company = $user.company
+                }
+          $usergroup += $entry
+      }
+    }
+ 
+  }  
+}
+
+write-host "Total Users: $($usergroup.count)"
+$usergroup | Group-Object -Property company | select name,count
+
+
+
+
+
---
+++ AD/Count SubGrup.ps1
@@ -0,0 +1,16 @@
+﻿(Get-ADUser -Filter * -Properties Office-Subgroup | where {(($_.Office-Subgroup -like "Office Shop%") -or ($_.Office-Subgroup -eq "Cluj") -or ($_.Office-Subgroup -eq "Suceava"))}).count
+
+(Get-ADUser -Filter {((Office-Subgroup -like "Office Shop%") -or (Office-Subgroup -eq "Cluj") -or (Office-Subgroup -eq "Suceava"))}).count
+
+(useraccountcontrol -ne "66050") -or (useraccountcontrol -ne "514") -and 
+
+(get-aduser -filter {Office-Subdepartment -like "Retail Channel"} -Properties Company, Name, EmployeeID, Office-Subgroup, msRTCSIP-UserEnabled | select Office-Subdepartment, Office-Subgroup, Name, EmployeeID, msRTCSIP-UserEnabled) | Export-Csv c:\temp\assets_$company.csv -NoTypeInformation
+
+
+.Office_Subgroup like "Office Shop%"
+Office_Subgroup = "Cluj"
+Office_Subgroup = "Suceava"
+msRTCSIP_UserEnabled 
+
+
+(get-aduser -filter {Office-Subdepartment -like "Retail Channel"} -Properties Company, Name, EmployeeID, Office-Subgroup, msRTCSIP-UserEnabled | where {(($_.'Office-Subgroup' -eq "Cluj") -or ($_.'Office-Subgroup' -like "Office Shop*") -or ($_.'Office-Subgroup' -eq "Suceava")) -and ($_.'msRTCSIP-UserEnabled' -eq $True)}).count
