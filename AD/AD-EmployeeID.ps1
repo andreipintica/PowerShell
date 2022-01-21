a77b146e9c266c1033941be58a7d1c176171522e
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 09:19:07 +0200

    Add files via upload

---
+++ AD/AD-EmployeeID.ps1
@@ -0,0 +1,7 @@
+﻿$Users = Import-Csv "c:\Temp\Marca_retail.csv"
+#ForEach ($Marca In $Users) Get-ADUser -Filter "EmployeeID -eq $($Users.Marca)" -Properties Name | select Name | Export-Csv C:\temp\user.csv -NoTypeInformation
+#Get-ADUser -Filter * -Properties Name,Title,mail,Office-DirectManager,Company,Office-Department,employeeID,office,Enabled,LastLogonDate | select SAmAccountName,Name,Title,mail,Office-DirectManager,Company,Office-Department,EmployeeID,office,Enabled,LastLogonDate, | Export-Csv C:\temp\user.csv -NoTypeInformation
+foreach($user in $users)
+{ 
+    Get-ADUser -Filter "EmployeeID -eq $($user.Marca)" -Properties SAMAccountName, EmployeeID | select SAMAccountName, EmployeeID #| Export-csv C:\temp\UserRetail.csv -NoTypeInformation 
+}
---
+++ AD/AD-User-Retail.ps1
@@ -0,0 +1,2 @@
+﻿get-aduser -filter {Office-Subdepartment -like "Retail Channel"} -Properties Company, Name, EmployeeID, Office-Subgroup, msRTCSIP-UserEnabled | where {($_.'Office-Subgroup' -eq "Skanska")} | select name | Sort-Object name
+
