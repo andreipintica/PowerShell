305f6bf4297e5c78f332c206fb58659677475423
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 10:50:08 +0200

    Update Excell_version.ps1

--- OS/Excell_version.ps1
+++ OS/Excell_version.ps1
@@ -3,7 +3,7 @@ foreach ($computer in (Get-Content C:\temp\servers2c.txt)){
  write-verbose "Working on $computer..." -Verbose
  reg query "HKEY_CLASSES_ROOT\Excel.Application\CurVer"
  }
-Export-CSV Select-Object Name,DistinguishedName, DNSHostName, OperatingSystem, Username | C:\Temp\tifui.csv
+Export-CSV Select-Object Name,DistinguishedName, DNSHostName, OperatingSystem, Username | C:\Temp\excell.csv
 
 
  
