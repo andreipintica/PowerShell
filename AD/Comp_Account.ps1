bfd93a435706f73375caf95127cbb9107bd78ae0
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 09:22:12 +0200

    Add files via upload

---
+++ AD/Comp_Account.ps1
@@ -0,0 +1,14 @@
+﻿ForEach ($Computer in (get-content "C:\temp\Computers.txt")) 
+{   Try {
+        Get-ADComputer $Computer -ErrorAction Stop
+        $Result = $true
+    }
+    Catch {
+        $Result = $False
+    }
+    [PSCustomObject]@{
+        Name = $server
+        Found = $Result
+    }
+}
+select Name,Enabled | Export-csv -path C:\temp\computer.csv -NoTypeInformation 
