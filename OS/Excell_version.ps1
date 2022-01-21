37cd18412f2821913bad7fa97b628c7f8b845e0f
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 10:19:51 +0200

    Add files via upload

---
+++ OS/Excell_version.ps1
@@ -0,0 +1,9 @@
+﻿Get-ADComputer "C:\Temp\servers2.txt" -Filter * -properties Name,DistinguishedName, DNSHostName, OperatingSystem, Username
+foreach ($computer in (Get-Content C:\temp\servers2c.txt)){
+ write-verbose "Working on $computer..." -Verbose
+ reg query "HKEY_CLASSES_ROOT\Excel.Application\CurVer"
+ }
+Export-CSV Select-Object Name,DistinguishedName, DNSHostName, OperatingSystem, Username | C:\Temp\tifui.csv
+
+
+ 
