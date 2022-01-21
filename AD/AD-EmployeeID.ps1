53adbf249da2f7243dcfb795dc33a5bd5d625762
Andrei Pintica <andrei.pintica@gmail.com>
2022-01-21 10:42:03 +0200

    Update AD-EmployeeID.ps1

--- AD/AD-EmployeeID.ps1
+++ AD/AD-EmployeeID.ps1
@@ -1,7 +1,7 @@
-﻿$Users = Import-Csv "c:\Temp\Marca_retail.csv"
-#ForEach ($Marca In $Users) Get-ADUser -Filter "EmployeeID -eq $($Users.Marca)" -Properties Name | select Name | Export-Csv C:\temp\user.csv -NoTypeInformation
+﻿$Users = Import-Csv "c:\Temp\EID_retail.csv"
+#ForEach ($Eid In $Users) Get-ADUser -Filter "EmployeeID -eq $($Users.Eid)" -Properties Name | select Name | Export-Csv C:\temp\user.csv -NoTypeInformation
 #Get-ADUser -Filter * -Properties Name,Title,mail,Office-DirectManager,Company,Office-Department,employeeID,office,Enabled,LastLogonDate | select SAmAccountName,Name,Title,mail,Office-DirectManager,Company,Office-Department,EmployeeID,office,Enabled,LastLogonDate, | Export-Csv C:\temp\user.csv -NoTypeInformation
 foreach($user in $users)
 { 
-    Get-ADUser -Filter "EmployeeID -eq $($user.Marca)" -Properties SAMAccountName, EmployeeID | select SAMAccountName, EmployeeID #| Export-csv C:\temp\UserRetail.csv -NoTypeInformation 
+    Get-ADUser -Filter "EmployeeID -eq $($user.Eid)" -Properties SAMAccountName, EmployeeID | select SAMAccountName, EmployeeID #| Export-csv C:\temp\UserEid.csv -NoTypeInformation 
 }
